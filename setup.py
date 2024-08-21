ad9b1323917d7a4ead4edd6092f6ed6b2d161666
Ofer Mendelevitch <ofermend@gmail.com>
2024-08-21 14:44:10 -0700

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_requirements():
 
 setup(
     name="vectara_agentic",
-    version="0.1.5",
+    version="0.1.6",
     author="Ofer Mendelevitch",
     author_email="ofer@vectara.com",
     description="A Python package for creating AI Assistants and AI Agents with Vectara",
