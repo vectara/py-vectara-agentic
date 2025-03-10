7e7b0326083a6b54bc109b1a30cbf484f96a81f4
david-oplatka <david.oplatka@vectara.com>
2025-03-10 16:23:05 -0700

    Remove code from old implementation

--- vectara_agentic/utils.py
+++ vectara_agentic/utils.py
@@ -12,7 +12,7 @@ from llama_index.core.llms import LLM
 from llama_index.llms.openai import OpenAI
 from llama_index.llms.anthropic import Anthropic
 
-from .types import LLMRole, AgentType, ModelProvider, AgentConfigType
+from .types import LLMRole, AgentType, ModelProvider
 from .agent_config import AgentConfig
 
 provider_to_default_model_name = {
@@ -28,15 +28,11 @@ provider_to_default_model_name = {
 
 DEFAULT_MODEL_PROVIDER = ModelProvider.OPENAI
 
-"""
-config_type (str): The agent configuration type, either "default" or "fallback"
-"""
 
 @lru_cache(maxsize=None)
 def _get_llm_params_for_role(
     role: LLMRole,
-    config: Optional[AgentConfig] = None,
-    config_type: Optional[AgentConfigType] = AgentConfigType.DEFAULT
+    config: Optional[AgentConfig] = None
 ) -> Tuple[ModelProvider, str]:
     """
     Get the model provider and model name for the specified role.
@@ -46,41 +42,22 @@ def _get_llm_params_for_role(
     config = config or AgentConfig()  # fallback to default config
 
     if role == LLMRole.TOOL:
-        if config_type == AgentConfigType.DEFAULT:
-            model_provider = config.tool_llm_provider
-            # If the user hasn’t explicitly set a tool_llm_model_name,
-            # fallback to provider default from provider_to_default_model_name
-            model_name = (
-                config.tool_llm_model_name
-                or provider_to_default_model_name.get(model_provider)
-            )
-        elif config_type == AgentConfigType.FALLBACK:
-            model_provider = config.fallback_tool_llm_provider
-            model_name = (
-                config.fallback_tool_llm_model_name
-                or provider_to_default_mode_name.get(model_provider)
-            )
-        else:
-            raise ValueError(f'Invalid agent configuration type {config_type}. Valid types are "default" or "fallback".')
+        model_provider = config.tool_llm_provider
+        # If the user hasn’t explicitly set a tool_llm_model_name,
+        # fallback to provider default from provider_to_default_model_name
+        model_name = (
+            config.tool_llm_model_name
+            or provider_to_default_model_name.get(model_provider)
+        )
     else:
-        if config_type == AgentConfigType.DEFAULT:
-            model_provider = config.main_llm_provider
-            model_name = (
-                config.main_llm_model_name
-                or provider_to_default_model_name.get(model_provider)
-            )
-        elif config_type == AgentConfigType.FALLBACK:
-            model_provider = config.fallback_main_llm_provider
-            model_name = (
-                config.fallback_main_llm_model_name
-                or provider_to_default_model_name.get(model_provider)
-            )
-        else:
-            raise ValueError(f'Invalid agent configuration type {config_type}. Valid types are "default" or "fallback".')
+        model_provider = config.main_llm_provider
+        model_name = (
+            config.main_llm_model_name
+            or provider_to_default_model_name.get(model_provider)
+        )
 
     # If the agent type is OpenAI, check that the main LLM provider is also OpenAI.
-    if role == LLMRole.MAIN and ((config.agent_type == AgentType.OPENAI and config_type == AgentConfigType.DEFAULT) or 
-                                (config.fallback_agent_type == AgentType.OPENAI and config_type == AgentConfigType.FALLBACK)):
+    if role == LLMRole.MAIN and config.agent_type == AgentType.OPENAI:
         if model_provider != ModelProvider.OPENAI:
             raise ValueError(
                 "OpenAI agent requested but main model provider is not OpenAI."
@@ -91,13 +68,12 @@ def _get_llm_params_for_role(
 @lru_cache(maxsize=None)
 def get_tokenizer_for_model(
     role: LLMRole,
-    config: Optional[AgentConfig] = None,
-    config_type: Optional[AgentConfigType] = AgentConfigType.DEFAULT
+    config: Optional[AgentConfig] = None
 ) -> Optional[Callable]:
     """
     Get the tokenizer for the specified model, as determined by the role & config.
     """
-    model_provider, model_name = _get_llm_params_for_role(role, config, config_type)
+    model_provider, model_name = _get_llm_params_for_role(role, config)
     if model_provider == ModelProvider.OPENAI:
         # This might raise an exception if the model_name is unknown to tiktoken
         return tiktoken.encoding_for_model(model_name).encode
@@ -109,14 +85,13 @@ def get_tokenizer_for_model(
 @lru_cache(maxsize=None)
 def get_llm(
     role: LLMRole,
-    config: Optional[AgentConfig] = None,
-    config_type: Optional[AgentConfigType] = AgentConfigType.DEFAULT
+    config: Optional[AgentConfig] = None
 ) -> LLM:
     """
     Get the LLM for the specified role, using the provided config
     or a default if none is provided.
     """
-    model_provider, model_name = _get_llm_params_for_role(role, config, config_type)
+    model_provider, model_name = _get_llm_params_for_role(role, config)
     if model_provider == ModelProvider.OPENAI:
         llm = OpenAI(model=model_name, temperature=0, is_function_calling_model=True)
     elif model_provider == ModelProvider.ANTHROPIC:
