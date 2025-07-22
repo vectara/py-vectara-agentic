c969ab6cf1e5d71468a6544d8d9c7b7c52cdbc6b
david-oplatka <david.oplatka@vectara.com>
2025-07-22 09:25:44 -0700

    Initial commit

--- vectara_agentic/tools.py
+++ vectara_agentic/tools.py
@@ -367,7 +367,8 @@ class VectaraToolFactory:
         frequency_penalty: Optional[float] = None,
         presence_penalty: Optional[float] = None,
         include_citations: bool = True,
-        citation_pattern: str = "{doc.url}",
+        citation_pattern: str = "{doc.url}", # WOULD LIKE TO CHANGE THIS TO citation_url_pattern, but this would be a breaking change
+        citation_text_pattern: str = "{doc.title}",
         save_history: bool = False,
         fcs_threshold: float = 0.0,
         return_direct: bool = False,
@@ -423,6 +424,9 @@ class VectaraToolFactory:
                 If True, uses markdown vectara citations that requires the Vectara scale plan.
             citation_pattern (str, optional): The pattern for the citations in the response.
                 Default is "{doc.url}" which uses the document URL.
+                If include_citations is False, this parameter is ignored.
+            citation_text_pattern (str, optional): The text pattern for citations in the response.
+                Default is "{doc.title}" which uses the title of the document.
                 If include_citations is False, this parameter is ignored.
             save_history (bool, optional): Whether to save the query in history.
             fcs_threshold (float, optional): A threshold for factual consistency.
@@ -502,6 +506,7 @@ class VectaraToolFactory:
                 presence_penalty=presence_penalty,
                 citations_style="markdown" if include_citations else None,
                 citations_url_pattern=citation_pattern if include_citations else None,
+                citations_text_pattern=citation_text_pattern if include_citations else None,
                 save_history=save_history,
                 x_source_str="vectara-agentic",
                 verbose=verbose,
